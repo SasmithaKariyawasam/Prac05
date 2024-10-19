7929c6b06b85be32db8a9febde9b56bfdb6c447e
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-10-19 13:46:38 +0530

    Add files via upload

---
+++ pr5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int width;
+    int height;
+    cout<<"Enter the width of the Rectangle: "<<endl;
+    cin>>width;
+    cout<<"Enter the height of the Rectangle: "<<endl;
+    cin>>height;
+    cout<<"Width: "<<width<<endl;
+    cout<<"Height: "<<height;
+    return 0;
+
+}
